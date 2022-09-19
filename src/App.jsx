5b1306b5ba1ebc2e161850545acefb19b7d2ce51
Md Farhad <freelancerfarhad12@gmail.com>
2022-09-19 15:16:05 +0600

    start projext

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
+import Gallery from './pages/gallery/Gallery';
+import Plans from './pages/palns/Plans';
+import Trainers from './pages/trainers/Trainers';
+import NotFound from './pages/notFound/NotFound';
+function App() {
+  return (
+    <>
+      <Home />
+      <About />
+      <Contact />
+      <Gallery />
+      <Plans />
+      <Trainers />
+      <NotFound />
+    </>
+  )
+}
+
+export default App
+
+
